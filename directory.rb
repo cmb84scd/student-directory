6236781fbf6dbf6aff0b4ef0ac5ddbfd4e42e277
Catriona Bennett <cmb84scd@gmail.com>
2020-04-08 16:30:21 +0000

    added interactive menu and some other minor changes

--- directory.rb
+++ directory.rb
@@ -1,17 +1,46 @@
+def interactive_menu
+  students = []
+  loop do
+    # print the menu and ask the user what to do
+    puts "1. Input  the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # read the input and save it into a variable
+    selection = gets.chomp
+    # do what the user has asked
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students and their cohort"
+  puts "To finish, just hit return three times"
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name, cohort = gets.chomp, gets.chomp.to_sym
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student to hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: cohort}
+    if students.count == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{students.count} students"
+    end
     # get another name from the user
-    name = gets.chomp
+    name, cohort = gets.chomp, gets.chomp.to_sym
   end
   # return the array of students
   students
@@ -23,8 +52,12 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  if students.count == 0
+    puts "There are no students enrolled"
+  else
+    students.each do |student|
+      puts "#{student[:name]}, (#{student[:cohort]} cohort)"
+    end
   end
 end
 
@@ -32,8 +65,4 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-#nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
